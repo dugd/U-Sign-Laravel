51890e6ad4acaf0e5221afad1b4767b8a93dcb69
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-09-06 09:20:08 +0300

    add common controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', [HomeController::class, 'index']);
+
+// Secure
+Route::get('/profile', [ProfileController::class, 'show']);
