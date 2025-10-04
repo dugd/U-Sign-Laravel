88ee104cb1ac625994896d6fc190c8cf7ef79db5
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-10-04 17:59:26 +0300

    build: setup tailwind, postcss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 @section('title', 'Головна')
 
 @section('content')
-    <h1>Welcome to USign!</h1>
-    <p>{{$welcome ?? "bimbim."}}</p>
+    <h1 class="font-bold text-black">Welcome to USign!</h1>
+    <p class="my-1">{{$welcome ?? "bimbim."}}</p>
 @endsection
