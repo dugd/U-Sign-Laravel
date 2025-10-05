c61718022cb41a9c59c330ca8cfb4fc1ac89b7a4
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-10-05 19:42:53 +0300

    feat: generate all need controllers

---
+++ app/Http/Controllers/Admin/GestureController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Gesture;
+use Illuminate\Http\Request;
+
+class GestureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Gesture $gesture)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Gesture $gesture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Gesture $gesture)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Gesture $gesture)
+    {
+        //
+    }
+}
