42bd5ea4c2e5f8a84bde097ee231d984df95b861
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-10-05 22:37:02 +0300

    new layout

--- resources/views/gestures/index.blade.php
+++ resources/views/gestures/index.blade.php
@@ -1,26 +1,30 @@
-@extends('layouts.default')
+<x-site-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6">
+                    <section class="text-center mb-10">
+                        <h1 class="text-3xl font-bold mb-2">Словник жестової мови</h1>
+                    </section>
 
-@section('title', 'Жести')
+                    <section>
+                        <h2 class="text-xl font-semibold mb-4">Перелік жестів:</h2>
 
-@section('content')
-    <section class="text-center mb-10">
-        <h1 class="text-3xl font-bold mb-2">Словник жестової мови</h1>
-    </section>
-
-    <section>
-        <h2 class="text-xl font-semibold mb-4">Перелік жестів:</h2>
-
-        <div class="grid gap-6 md:grid-cols-3">
-            @forelse($gestures as $gesture)
-                <a href="{{ route('gestures.show', $gesture->id) }}" class="block bg-white shadow rounded p-4 hover:shadow-md">
-                    <video src="{{ $gesture->video_path }}" class="w-full rounded mb-2" controls></video>
-                    <h3 class="text-xl font-medium">{{ $gesture->title }}</h3>
-                    <p class="">{{ trim(mb_substr($gesture->description, 0, 100)) . "..." }}</p>
-                    <p class="text-sm text-gray-500">{{ $gesture->language_code }}</p>
-                </a>
-            @empty
-                <p class="text-gray-500">Нема :(</p>
-            @endforelse
+                        <div class="grid gap-6 md:grid-cols-3">
+                            @forelse($gestures as $gesture)
+                                <a href="{{ route('gestures.show', $gesture->id) }}" class="block bg-white shadow rounded p-4 hover:shadow-md">
+                                    <video src="{{ $gesture->video_path }}" class="w-full rounded mb-2" controls></video>
+                                    <h3 class="text-xl font-medium">{{ $gesture->title }}</h3>
+                                    <p class="">{{ trim(mb_substr($gesture->description, 0, 100)) . "..." }}</p>
+                                    <p class="text-sm text-gray-500">{{ $gesture->language_code }}</p>
+                                </a>
+                            @empty
+                                <p class="text-gray-500">Нема :(</p>
+                            @endforelse
+                        </div>
+                    </section>
+                </div>
+            </div>
         </div>
-    </section>
-@endsection
+    </div>
+</x-site-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,22 @@
-@extends('layouts.default')
+<x-site-layout>
 
-@section('title', 'Головна')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6">
+                    <section class="text-center mb-10">
+                        <h1 class="text-3xl font-bold mb-2">Словник жестової мови</h1>
+                    </section>
 
-@section('content')
-    <section class="text-center mb-10">
-        <h1 class="text-3xl font-bold mb-2">Словник жестової мови</h1>
-    </section>
+                    <section>
+                        <h2 class="text-xl font-semibold mb-4">Останні додані жести:</h2>
 
-    <section>
-        <h2 class="text-xl font-semibold mb-4">Останні додані жести:</h2>
-
-        <div class="grid gap-6 md:grid-cols-3">
-            <p class="text-gray-500">Нема :(</p>
+                        <div class="grid gap-6 md:grid-cols-3">
+                            <p class="text-gray-500">Нема :(</p>
+                        </div>
+                    </section>
+                </div>
+            </div>
         </div>
-    </section>
-@endsection
+    </div>
+</x-site-layout>
