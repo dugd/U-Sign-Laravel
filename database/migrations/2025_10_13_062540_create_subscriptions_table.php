d8c5272b6d4c11f405d307eb8aca0a2c3be08d1c
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-10-13 09:31:17 +0300

    feat: create subscriptions table migration

---
+++ database/migrations/2025_10_13_062540_create_subscriptions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use \Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
+            $table->enum('plan', ['vip', 'pro']);
+
+            $table->timestamp('start_date')->useCurrent();
+            $table->timestamp('end_date')->nullable();
+            $table->boolean('cancel_date')->nullable();
+
+            $table->json('meta')->nullable();
+            $table->timestamps();
+
+            // indexes mega optimization
+            $table->index(['user_id','plan']);
+            $table->index(['user_id','start_date']);
+            $table->index(['user_id','end_date']);
+        });
+        DB::statement('ALTER TABLE subscriptions ADD CONSTRAINT chk_dates CHECK (end_date is null or start_date < end_date);');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
